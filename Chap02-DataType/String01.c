0c88f45db2d56a08248573464de1f215d61c7529
CJH <excelsiorcjh@gmail.com>
2018-04-25 09:08:48 +0900

    코드 수정

--- Chap02-DataType/String01.c
+++ Chap02-DataType/String01.c
@@ -5,11 +5,9 @@ int main(void)
     char c1 = 'A', ch2 = 'B', ch3 = 'C';
     char szData[4] = {'A', 'B', 'C'};
     char szNewData[4] = {"ABC"};
-    printf("%s", szData);
-    printf("\n");
-    printf("%s", szNewData);
-    printf("\n");
+    printf("%s\n", szData);
+    printf("%s\n", szNewData);
     // Array에 명시된 값이 없으면 0으로 초기화
-    printf("%d", szData[3]);
+    printf("%d\n", szData[3]);
     return 0;    
 }
